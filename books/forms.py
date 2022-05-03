7f5b5a3ae5bffb2136be9a6ac44bce38041fe861
Uzytkownik <uzytkownik@kursGIT.pl>
2022-05-03 08:56:06 +0200

    Forms

--- books/forms.py
+++ books/forms.py
@@ -1,16 +1,7 @@
-
-from django.forms import Form, CharField
-import logging
-
-logger = logging.getLogger("formsLogger")
-
 from django.forms import Form, CharField, ModelForm
 import logging
 
-from books.models import BookAuthor, Book
-
-logger = logging.getLogger("forms_logger")
-
+logger = logging.getLogger("formsLogger")
 
 class CategoryForm(Form):
     name = CharField(max_length=156, required=True)
@@ -20,36 +11,4 @@ class CategoryForm(Form):
         return self.cleaned_data.get("name")
 
 
-class AuthorForm(ModelForm):
-    class Meta():
-        model = BookAuthor
-        exclude = []
-
-    name = CharField(min_length=3, max_length=256, required=True)
-
-    def clean(self):
-        result = super().clean()
-        logger.info(f"AuthorForm - clean run - result is {result}")
-        return result
-
-
-class BookForm(ModelForm):
-    class Meta:
-        model = Book
-        # exclude = []
-        fields = [
-            "title",
-            "authors",
-            "publisher",
-            "published_date",
-            "categories",
-            "average_rating",
-        ]
-
-    title = CharField(min_length=3, max_length=256, required=True)
-
-    def clean(self):
-        result = super().clean()
-        logger.info(f"BookForm - clean run - result is {result}")
-        return result
 
--- books/urls.py
+++ books/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from books.views import get_uuids_a, get_uuids_b, get_argument_from_path, get_arguments_from_query, \
     AuthorListBaseView, raise_error_for_fun, get_headers, check_http_query_type, CategoryListTemplateView, \
-    BooksListView, BooksDetailsView
+    BooksListView, BooksDetailsView, CategoryCreateFormView
 
 
 class BooksListTemplateView:
@@ -17,9 +17,10 @@ urlpatterns = [
     path('check-type', check_http_query_type, name="get_from_type"),
     path('get-headers',get_headers, name="get_headers"),
     path('raise-error', raise_error_for_fun, name="raise-error"),
-    path('author-list', AuthorListBaseView.as_view(), name="author-list"), # pierwsze author-list wpisujemy do przeglądarki , A drugie odwołujemy sie w kodzie.
+    path('author-list', AuthorListBaseView.as_view(), name="author-list"), # pierwsze author-list wpisujemy do przeglądarki , A drugie odwołujemy sie w kodzie(to co szuka django)
     path('category-list', CategoryListTemplateView.as_view(), name="category-list"),
     path('books-list', BooksListView.as_view(), name="books-list"),
     path('book-details/<int:pk>/', BooksDetailsView.as_view(), name="books-details"),
+    path('category-create', CategoryCreateFormView.as_view(), name="category-create"),
 
 ]
